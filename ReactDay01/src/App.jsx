af8272db31d4e256aa1cc6751e1f36f08c833700
Ranjit Kumar Singh <ranjitksingh.079@gmail.com>
2025-09-11 23:46:34 +0530

    React Day 01

---
+++ ReactDay01/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+function App() {
+  
+
+  return (
+    <div>
+      <Navbar />
+    <p>
+      I had started learning React.js from 
+      Love Babbar becasue i didn't get anything in react from Udemy course and still have lots of confusion in almost every topic.
+    </p>
+    </div>
+  )
+}
+
+export default App
---
+++ ReactDay01/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>📌Attention Everyone</h1>
+    </div>
+  )
+}
+
+export default Navbar;
